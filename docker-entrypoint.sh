875b7238eb248342a4a52e758e4d99cfff0447a7
Jan Meckelholt <Jan_Meckelholt@web.de>
2023-03-25 21:26:45 +0100

    fix Dockerfiles

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 # Abort on any error (including if wait-for-it fails). set -e
 # Wait for strava-service
 
- /go/src/app/wait-for-it.sh "strava-servcie:666" -t 20
+ /go/src/running/wait-for-it.sh "strava-service:666" -t 20
 
  # Run the main container command.
 exec "$@"
