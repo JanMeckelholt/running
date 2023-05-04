ccc1478f1047811d974bcf28c0fd024cd9e14e2e
JanMeckelholt <Jan_Meckelholt@web.de>
2023-05-04 12:37:09 +0200

    port changes

--- running_app/lib/constants.dart
+++ running_app/lib/constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   static const String baseURL = 'localhost';
   static const String baseURLAndroidEmulator = '10.0.2.2';
-  static const int port = 80;
+  static const int port = 81;
   static const String summaryPath = '/weeksummary';
   static const String loginPath = '/website';
   static const String clientId = '77376';
