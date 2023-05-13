30801a1cdde5298f05e85f15fa6f4a90dc08d2d9
JanMeckelholt <Jan_Meckelholt@web.de>
2023-05-13 07:47:07 +0200

    https for server.py

---
+++ running_app/server.py
@@ -0,0 +1,10 @@
+import http.server, ssl
+
+server_address = ('localhost', 5000)
+httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket,
+                               server_side=True,
+                               certfile='./assets/certs/running_app-cert.pem',
+                               keyfile='./assets/certs/running_app-key.pem',
+                               ssl_version=ssl.PROTOCOL_TLS)
+httpd.serve_forever()
--- running_app/server.sh
+++ running_app/server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 PORT=5000
 
+mv server.py ./build/web/
 cd build/web/
 
+
 echo 'Server starting on port' $PORT '...'
-python3 -m http.server $PORT
+python3 server.py
