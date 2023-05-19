6ca001d82d918698eef22bfe5608ac2d20744588
JanMeckelholt <Jan_Meckelholt@web.de>
2023-05-19 22:00:36 +0200

    fix docker-publish

--- buildArm64.sh
+++ buildArm64.sh
@@ -7,7 +7,7 @@ docker tag running_runner janmeckelholt/running_runner:$(git rev-parse main)_arm
 docker tag running_strava-service janmeckelholt/running_strava-service:$(git rev-parse main)_arm64 && \
 docker tag running_database-service janmeckelholt/running_database-service:$(git rev-parse main)_arm64 && \
 docker tag running_running_app janmeckelholt/running_running_app:$(git rev-parse main)_arm64 && \
-docker tag running_postgres janmeckelholt/running_postgres:$(git rev-parse main)_arm64
+docker tag running_postgres janmeckelholt/running_postgres:$(git rev-parse main)_arm64 && \
 echo $1 | docker login -u janmeckelholt --password-stdin
 docker push janmeckelholt/running_http_gateway:$(git rev-parse main)_arm64
 docker push janmeckelholt/running_runner:$(git rev-parse main)_arm64
