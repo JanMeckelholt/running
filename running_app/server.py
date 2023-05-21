698d2489bf614d80bc53700e7af878c482620f7d
JanMeckelholt <Jan_Meckelholt@web.de>
2023-05-21 19:28:27 +0200

    write logs for server.py

--- running_app/server.py
+++ running_app/server.py
@@ -1,11 +1,18 @@
 import http.server, ssl, socketserver
+import sys
+buffer = 1
 
 context =  ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 context.load_cert_chain(certfile='./assets/certs/running_app-cert.pem', keyfile='./assets/certs/running_app-key.pem')
 handler = http.server.BaseHTTPRequestHandler
 server_address = ('0.0.0.0', 5000)
+
+
+
+
 with socketserver.TCPServer(server_address, handler) as httpd:
     httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+    sys.stderr = open('server_logfile.txt', 'w', buffer)
     httpd.serve_forever()
 
 
