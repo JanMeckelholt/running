fc74d092a2c28da0571e07b1b750b363b90598c3
JanMeckelholt <Jan_Meckelholt@web.de>
2023-06-03 21:20:43 +0200

    add homepage-server to cors

--- http_gateway/service/server/server.go
+++ http_gateway/service/server/server.go
@@ -55,12 +55,14 @@ func (rs HttpGatewayServer) GetWeekSummary(ctx context.Context, request runner.W
 
 func CorsMiddleware(next http.Handler, config config.ServiceConfig) http.Handler {
 	allowList := map[string]bool{
-		"janmeckelholt.de":                                         true,
-		"https://janmeckelholt.de":                                 true,
-		"https://janmeckelholt.de/run":                             true,
-		"https://janmeckelholt.de/run/":                            true,
-		fmt.Sprintf("http://localhost:%d", config.RunningAppPort):  true,
-		fmt.Sprintf("https://localhost:%d", config.RunningAppPort): true,
+		"janmeckelholt.de":                                               true,
+		"https://janmeckelholt.de":                                       true,
+		"https://janmeckelholt.de/run":                                   true,
+		"https://janmeckelholt.de/run/":                                  true,
+		fmt.Sprintf("http://homepage-server:%d", config.RunningAppPort):  true,
+		fmt.Sprintf("https://homepage-server:%d", config.RunningAppPort): true,
+		fmt.Sprintf("http://localhost:%d", config.RunningAppPort):        true,
+		fmt.Sprintf("https://localhost:%d", config.RunningAppPort):       true,
 	}
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
