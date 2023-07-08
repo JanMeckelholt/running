7f7026bf5d8b3e7fd8233e6c3667c9c3d5496e97
JanMeckelholt <Jan_Meckelholt@web.de>
2023-07-08 10:30:14 +0200

    increase decrease working

--- runner/service/frontend/frontend.go
+++ runner/service/frontend/frontend.go
@@ -20,9 +20,22 @@ var (
 	tmpl *template.Template
 	err  error
 )
+var templPath = "./runner/service/frontend/html-templates/"
+var templFile = "runner.html"
+var funcMap = template.FuncMap{
+	"dec": func(Week int64) int64 {
+		return Week - 1
+	},
+	"inc": func(Week int64) int64 {
+		if Week >= 0 {
+			return 0
+		}
+		return Week + 1
+	},
+}
 
 func init() {
-	tmpl, err = template.ParseFiles("./runner/service/frontend/html-templates/runner.html")
+	tmpl, err = template.New(templFile).Funcs(funcMap).ParseFiles(templPath + templFile)
 	if err != nil {
 		panic(err)
 	}
@@ -40,6 +53,9 @@ func FrontEnd(rs runner.RunnerServer) http.HandlerFunc {
 			log.Errorf("Error getting Weeksummary: %s", err.Error())
 		}
 		err = tmpl.Execute(w, TemplateData{RunningData: data, Week: int64(weekInt)})
+		if err != nil {
+			log.Errorf("Could not parse template: %s", err.Error())
+		}
 	}
 
 }
