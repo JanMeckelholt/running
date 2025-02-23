6ac1186661221b08c29ce2da4675560f4db5887d
JanMeckelholt <Jan_Meckelholt@web.de>
2025-02-23 15:03:01 +0100

    change to amd

---
+++ buildx86.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 PLATFORM=linux/amd64 docker-compose build
+
+docker tag running_http_gateway janmeckelholt/running_http_gateway:$(git rev-parse main)_amd64  && \
+docker tag running_runner janmeckelholt/running_runner:$(git rev-parse main)_amd64 && \
+docker tag running_strava-service janmeckelholt/running_strava-service:$(git rev-parse main)_amd64 && \
+docker tag running_database-service janmeckelholt/running_database-service:$(git rev-parse main)_amd64 && \
+docker tag running_running_app janmeckelholt/running_running_app:$(git rev-parse main)_amd64 && \
+docker tag running_postgres janmeckelholt/running_postgres:$(git rev-parse main)_amd64 && \
+echo $1 | docker login -u janmeckelholt --password-stdin
+docker push janmeckelholt/running_http_gateway:$(git rev-parse main)_amd64
+docker push janmeckelholt/running_runner:$(git rev-parse main)_amd64
+docker push janmeckelholt/running_strava-service:$(git rev-parse main)_amd64
+docker push janmeckelholt/running_database-service:$(git rev-parse main)_amd64
+docker push janmeckelholt/running_running_app:$(git rev-parse main)_amd64
+docker push janmeckelholt/running_postgres:$(git rev-parse main)_amd64
