e5d7df6893395644077b769cd09c39b0652ba790
JanMeckelholt <Jan_Meckelholt@web.de>
2025-03-09 08:46:28 +0100

    remove unused ports from http_gateway

--- common/dependencies/dependencies.go
+++ common/dependencies/dependencies.go
@@ -8,7 +8,6 @@ var Configs = map[string]Config{
 	"strava-service":   {Port: 666},
 	"database-service": {Port: 666},
 	"http_gateway-API": {Port: 333},
-	"http_gateway":     {Port: 80},
 	"runner":           {Port: 666},
 	"runner_frontend":  {Port: 443},
 	"populate_db":      {Port: 666},
